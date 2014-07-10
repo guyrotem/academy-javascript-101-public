a3277f3468b605a4c5a1a13209b20aaf78bdd219
Omry Nachman <omry@wix.com>
2014-07-10 13:20:14 +0300

    solution

--- src/prototype.js
+++ src/prototype.js
@@ -1,10 +1,41 @@
 "use strict";
 
 var Animal = function (name) {
+  if (name) {
+    this.name = name;
+  }
 };
 
+Animal.prototype.speak = function () {
+  return this.sound;
+};
+
+Animal.prototype.toString = function() {
+  return this.name + ' is ' + this.getType() + ' with ' + this.legs + ' legs that says "' + this.speak() + '"';
+};
+
+Animal.prototype.getType = function() {
+  return 'an animal';
+};
+
+Animal.prototype.name = 'Default';
+Animal.prototype.sound = 'Kukukuchoo';
+Animal.prototype.legs = 100;
+
 var Mammal = function (name) {
+  Animal.apply(this, arguments);
+};
+Mammal.prototype = new Animal();
+Mammal.prototype.legs = 4;
+Mammal.prototype.getType = function(){
+  return 'a mammal';
 };
 
 var Dog = function (name) {
+  Mammal.apply(this, arguments);
+};
+Dog.prototype = new Mammal();
+Dog.prototype.sound = 'Woof';
+Dog.prototype.getType = function(){
+  return 'a dog';
 };
