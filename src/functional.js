275c2e4b5f375d095b127792f2b7dcc3722a3707
Omry Nachman <omry@wix.com>
2014-07-14 10:34:47 +0300

    full solution for createAllOfFilter

--- src/functional.js
+++ src/functional.js
@@ -9,6 +9,19 @@ function createMultiplier(multiplyBy) {
 }
 
 function createAllOfFilter(conditionsArray){
+  if (conditionsArray && conditionsArray.length) {
+    conditionsArray = conditionsArray.filter(function(condition){
+      return condition instanceof Function;
+    });
+
+    return function(item){
+      return conditionsArray.every(function(condition){
+        return condition(item);
+      })
+    }
+  }
+
+
   return function trivialTrueFilter(){
     return true;
   }
